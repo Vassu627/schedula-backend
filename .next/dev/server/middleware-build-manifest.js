cc4fb2d37f18d4972b089df1de123e517b2396fb
Vassu627 <vasanthar627@gmail.com>
2026-02-05 19:05:39 +0530

    Add PostgreSQL database connection setup

--- .next/dev/server/middleware-build-manifest.js
+++ .next/dev/server/middleware-build-manifest.js
@@ -1,6 +1,31 @@
 globalThis.__BUILD_MANIFEST = {
   "pages": {
-    "/_app": []
+    "/_app": [
+      "static/chunks/fd94d_next_dist_compiled_ef7984cd._.js",
+      "static/chunks/fd94d_next_dist_shared_lib_626f2e73._.js",
+      "static/chunks/fd94d_next_dist_client_139af99e._.js",
+      "static/chunks/fd94d_next_dist_2737e935._.js",
+      "static/chunks/fd94d_next_app_e9dd45a8.js",
+      "static/chunks/[next]_entry_page-loader_ts_64d169d5._.js",
+      "static/chunks/fd94d_react-dom_16e51f6f._.js",
+      "static/chunks/fd94d_61b1d25b._.js",
+      "static/chunks/[root-of-the-server]__c4a53a5d._.js",
+      "static/chunks/schedula-backend_pages__app_2da965e7._.js",
+      "static/chunks/turbopack-schedula-backend_pages__app_330c6e5a._.js"
+    ],
+    "/_error": [
+      "static/chunks/fd94d_next_dist_compiled_ef7984cd._.js",
+      "static/chunks/fd94d_next_dist_shared_lib_c5bbf75a._.js",
+      "static/chunks/fd94d_next_dist_client_139af99e._.js",
+      "static/chunks/fd94d_next_dist_990257c7._.js",
+      "static/chunks/fd94d_next_error_3035e627.js",
+      "static/chunks/[next]_entry_page-loader_ts_a1655e42._.js",
+      "static/chunks/fd94d_react-dom_16e51f6f._.js",
+      "static/chunks/fd94d_61b1d25b._.js",
+      "static/chunks/[root-of-the-server]__6b95fce2._.js",
+      "static/chunks/schedula-backend_pages__error_2da965e7._.js",
+      "static/chunks/turbopack-schedula-backend_pages__error_2cb2f919._.js"
+    ]
   },
   "devFiles": [],
   "polyfillFiles": [],
